238a182b932bdea7dca48022ff4e979535e25a66
kadirkaracan <101510637+kadirkaracan@users.noreply.github.com>
2022-03-21 13:50:42 +0300

    ka
    ka

---
+++ ders 4/src/ders4.java
@@ -0,0 +1,17 @@
+
+public class ders4 {
+
+	public static void main(String[] args) {
+		// çok büyük sayýlar içindir. 8byte
+		
+		/*
+		long maxdeger = Long.MAX_VALUE ;
+		long mindeger = Long.MIN_VALUE ;
+		 System.out.println(maxdeger);
+		 System.out.println(mindeger);
+			*/
+long sayi = 285765258993758L ;
+System.out.println(sayi);
+	}
+
+}
