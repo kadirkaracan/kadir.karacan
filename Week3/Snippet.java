2d160352c28e9b5b7299c177afe55a5954fdc07e
kadirkaracan <101510637+kadirkaracan@users.noreply.github.com>
2022-03-30 17:58:29 +0300

    kadir karacan 21640310019
    kadir karacan 21640310019

---
+++ Week3/Snippet.java
@@ -0,0 +1,6 @@
+package snippet;
+
+public class Snippet {
+	double vize2 = scanner.nextDouble();
+}
+
---
+++ Week3/week11.java
@@ -0,0 +1,20 @@
+package parametrelifonksiyonlar;
+
+public class week11 {
+public static void toplama(int a, int b,int c) {
+	
+	
+	
+	System.out.println("toplamlarý:" + (a+b+c));
+	
+	
+	
+}
+	
+	
+	public static void main(String[] args) {
+		toplama(3, 4, 5);
+		toplama(29, 45, 52);
+	}
+
+}
