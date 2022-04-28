4931ae07871da7be3c3db0e92c00da773081f47b
kadirkaracan <101510637+kadirkaracan@users.noreply.github.com>
2022-04-28 19:49:35 +0300

    Kadir Karacan 21640310019
    Kadir Karacan 21640310019

---
+++ Çalışmalar/Account.java
@@ -0,0 +1,18 @@
+package account;
+
+public class Account {
+String name;
+int money;
+
+public Account (String name, int money) {
+	this.name=name;
+	this.money=money;
+}
+
+public String toString() {
+	return name+": $" + money;
+	
+}
+
+
+	}
