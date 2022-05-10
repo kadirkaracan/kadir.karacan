ebb9e77e73efd061a56f15fb8cb4632add50d0b1
kadirkaracan <101510637+kadirkaracan@users.noreply.github.com>
2022-05-10 16:23:57 +0300

    Kadir Karacan 21640310019
    Kadir Karacan 21640310019
    yatay geçişle geldiğim için bu dönemdeki ilk çalişmalarım hocam.

---
+++ Çalışmalar/test1.java
@@ -0,0 +1,15 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// tooltip
+		// breakpoint
+		int sayi1 =8;
+		int sayi2= 10;
+		int toplam=sayi1 +sayi2;
+		int sonuc = (toplam/2)*5;
+		System.out.println(sonuc);
+
+	}
+
+}
